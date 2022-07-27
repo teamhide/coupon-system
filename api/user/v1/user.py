c319129dfe2c9c399ef40f62cd4324e0a7c704a2
Hide <padocon@naver.com>
2022-07-27 14:16:11 +0900

    Init

---
+++ api/user/v1/user.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, Query
+
+from app.user.schemas import (
+    ExceptionResponseSchema,
+    GetUserListResponseSchema,
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+)
+from app.user.services import UserService
+from core.fastapi.dependencies import (
+    PermissionDependency,
+    IsAdmin,
+)
+
+user_router = APIRouter()
+
+
+@user_router.get(
+    "",
+    response_model=List[GetUserListResponseSchema],
+    response_model_exclude={"id"},
+    responses={"400": {"model": ExceptionResponseSchema}},
+    dependencies=[Depends(PermissionDependency([IsAdmin]))],
+)
+async def get_user_list(
+    limit: int = Query(10, description="Limit"),
+    prev: int = Query(None, description="Prev ID"),
+):
+    return await UserService().get_user_list(limit=limit, prev=prev)
+
+
+@user_router.post(
+    "",
+    response_model=CreateUserResponseSchema,
+    responses={"400": {"model": ExceptionResponseSchema}},
+)
+async def create_user(request: CreateUserRequestSchema):
+    await UserService().create_user(**request.dict())
+    return {"email": request.email, "nickname": request.nickname}
---
+++ app/user/services/user.py
@@ -0,0 +1,59 @@
+from typing import Optional, List, Union, NoReturn
+
+from sqlalchemy import or_, select
+
+from app.user.models import User
+from core.db import Transactional, Propagation, session
+from core.exceptions import (
+    PasswordDoesNotMatchException,
+    DuplicateEmailOrNicknameException,
+)
+
+
+class UserService:
+    def __init__(self):
+        pass
+
+    async def get_user_list(
+        self,
+        limit: int = 12,
+        prev: Optional[int] = None,
+    ) -> List[User]:
+        query = select(User)
+
+        if prev:
+            query = query.where(User.id < prev)
+
+        if limit > 12:
+            limit = 12
+
+        query = query.limit(limit)
+        result = await session.execute(query)
+        return result.scalars().all()
+
+    @Transactional(propagation=Propagation.REQUIRED)
+    async def create_user(
+        self, email: str, password1: str, password2: str, nickname: str
+    ) -> None:
+        if password1 != password2:
+            raise PasswordDoesNotMatchException
+
+        query = select(User).where(or_(User.email == email, User.nickname == nickname))
+        result = await session.execute(query)
+        is_exist = result.scalars().first()
+        if is_exist:
+            raise DuplicateEmailOrNicknameException
+
+        user = User(email=email, password=password1, nickname=nickname)
+        session.add(user)
+
+    async def is_admin(self, user_id: int) -> bool:
+        result = await session.execute(select(User).where(User.id == user_id))
+        user = result.scalars().first()
+        if not user:
+            return False
+
+        if user.is_admin is False:
+            return False
+
+        return True
