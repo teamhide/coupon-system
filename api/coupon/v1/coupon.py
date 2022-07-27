cae13da486e601218cee0f7d9c41028c492c05c6
Hide <padocon@naver.com>
2022-07-27 19:05:01 +0900

    Fix docker config

--- api/coupon/v1/coupon.py
+++ api/coupon/v1/coupon.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Request, Depends, Response
 
+from app.coupon.service.coupon import CouponService
+from core.fastapi.dependencies import PermissionDependency, IsAuthenticated
 
 coupon_router = APIRouter()
 
 
-@coupon_router.post("")
-async def create_coupon():
-    return {}
+@coupon_router.post(
+    "",
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+)
+async def get_coupon(request: Request):
+    await CouponService().get_coupon(user_id=request.user.id)
+    return Response(status_code=200)
---
+++ app/coupon/service/coupon.py
@@ -0,0 +1,3 @@
+class CouponService:
+    async def get_coupon(self, user_id: int) -> None:
+        print(1)
